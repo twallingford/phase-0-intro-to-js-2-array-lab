c739f5ad64340d8ea5b179d9e36db9a79a984856
Thomas Wallingford <tbwallingford@gmail.com>
2022-12-04 18:13:04 -0700

    commit tree eae37d4f27e4f84914cf2f667e1ba70a782317d0

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat(name) {
+    const newCats = [
+        ...cats,
+        name
+    ];
+    return newCats;
+}
+
+function prependCat(name) {
+    const newCats = [
+        name,
+        ...cats
+    ];
+    return newCats;
+}
+
+function removeLastCat() {
+    return cats.slice(0,-1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
